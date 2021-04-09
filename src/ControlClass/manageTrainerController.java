ad8be816ab50a9edb1bc3d7d255f42fa2b51a566
GY <guchucheng2000@gmail.com>
2021-04-09 21:03:09 +0800

    Update manage_user.fxml, manage_trainer.fxml and manageTrainerController.java

---
+++ src/ControlClass/manageTrainerController.java
@@ -0,0 +1,304 @@
+package ControlClass;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TextField;
+
+public class manageTrainerController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label email1_mt;
+
+    @FXML
+    private Hyperlink regu_mt;
+
+    @FXML
+    private TextField text_find_mt;
+
+    @FXML
+    private Label id3_mt;
+
+    @FXML
+    private Button button_delete_mt;
+
+    @FXML
+    private Hyperlink trainer_hyplink_mt;
+
+    @FXML
+    private Label add_email_mt;
+
+    @FXML
+    private Label password2_mt;
+
+    @FXML
+    private Label password4_mt;
+
+    @FXML
+    private Label id1_mt;
+
+    @FXML
+    private Hyperlink out_hyplink_mt;
+
+    @FXML
+    private Label name3_mt;
+
+    @FXML
+    private Label email3_mt;
+
+    @FXML
+    private CheckBox box4_mt;
+
+    @FXML
+    private Pagination turn_page_mt;
+
+    @FXML
+    private Hyperlink income_hyplink_mt;
+
+    @FXML
+    private Button button_edit_mt;
+
+    @FXML
+    private Label password_mt;
+
+    @FXML
+    private CheckBox box2_mt;
+
+    @FXML
+    private Label add_password_mt;
+
+    @FXML
+    private Label email2_mt;
+
+    @FXML
+    private Label name1_mt;
+
+    @FXML
+    private Label id2_mt;
+
+    @FXML
+    private Label id4_mt;
+
+    @FXML
+    private Label password1_mt;
+
+    @FXML
+    private Label password3_mt;
+
+    @FXML
+    private Label email_mt;
+
+    @FXML
+    private Hyperlink user_hyplink_mt;
+
+    @FXML
+    private Label add_name_mt;
+
+    @FXML
+    private Label name_mt;
+
+    @FXML
+    private Label name2_mt;
+
+    @FXML
+    private Label email4_mt;
+
+    @FXML
+    private Button add_trainer_mt;
+
+    @FXML
+    private Label name4_mt;
+
+    @FXML
+    private Label id_mt;
+
+    @FXML
+    private Button button_find_mt;
+
+    @FXML
+    private CheckBox box3_mt;
+
+    @FXML
+    private CheckBox box1_mt;
+
+    @FXML
+    void click_user_hyplink_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_trainer_hyplink_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_income_hyplink_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_regu_hyplink_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_out_hyplink_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_add_name_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_add_email_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_add_password_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name1_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email1_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name2_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name3_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name4_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email2_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email3_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email4_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password1_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password2_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password3_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password4_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_add_trainer_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_find_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_edit_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_delete_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_turn_page_mt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert email1_mt != null : "fx:id=\"email1_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert regu_mt != null : "fx:id=\"regu_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert text_find_mt != null : "fx:id=\"text_find_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert id3_mt != null : "fx:id=\"id3_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert button_delete_mt != null : "fx:id=\"button_delete_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert trainer_hyplink_mt != null : "fx:id=\"trainer_hyplink_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert add_email_mt != null : "fx:id=\"add_email_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert password2_mt != null : "fx:id=\"password2_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert password4_mt != null : "fx:id=\"password4_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert id1_mt != null : "fx:id=\"id1_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert out_hyplink_mt != null : "fx:id=\"out_hyplink_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert name3_mt != null : "fx:id=\"name3_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert email3_mt != null : "fx:id=\"email3_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert box4_mt != null : "fx:id=\"box4_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert turn_page_mt != null : "fx:id=\"turn_page_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert income_hyplink_mt != null : "fx:id=\"income_hyplink_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert button_edit_mt != null : "fx:id=\"button_edit_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert password_mt != null : "fx:id=\"password_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert box2_mt != null : "fx:id=\"box2_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert add_password_mt != null : "fx:id=\"add_password_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert email2_mt != null : "fx:id=\"email2_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert name1_mt != null : "fx:id=\"name1_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert id2_mt != null : "fx:id=\"id2_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert id4_mt != null : "fx:id=\"id4_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert password1_mt != null : "fx:id=\"password1_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert password3_mt != null : "fx:id=\"password3_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert email_mt != null : "fx:id=\"email_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert user_hyplink_mt != null : "fx:id=\"user_hyplink_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert add_name_mt != null : "fx:id=\"add_name_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert name_mt != null : "fx:id=\"name_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert name2_mt != null : "fx:id=\"name2_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert email4_mt != null : "fx:id=\"email4_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert add_trainer_mt != null : "fx:id=\"add_trainer_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert name4_mt != null : "fx:id=\"name4_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert id_mt != null : "fx:id=\"id_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert button_find_mt != null : "fx:id=\"button_find_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert box3_mt != null : "fx:id=\"box3_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+        assert box1_mt != null : "fx:id=\"box1_mt\" was not injected: check your FXML file 'manage_trainer.fxml'.";
+
+    }
+}
