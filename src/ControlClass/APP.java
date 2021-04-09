e6759bd4da562c0f9f5933e2f40cc6d59159bb60
GY <guchucheng2000@gmail.com>
2021-04-09 10:01:55 +0800

    Update fxml: register

--- src/ControlClass/APP.java
+++ src/ControlClass/APP.java
@@ -8,12 +8,8 @@ import javafx.stage.Stage;
 public class APP extends javafx.application.Application{
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
-
-//        Image image=new Image("../resource/login.png");
-//        pane.getChildren().add(new ImageView(image));
-
-
+        //Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/register.fxml"));
         Scene scene = new Scene(root, 1350, 807);
         stage.setScene(scene);
         stage.show();
