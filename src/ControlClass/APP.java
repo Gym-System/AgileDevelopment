8f87e83e6304a7d1fe103b27b9907517e3d8dd63
xyx <2460249547@qq.com>
2021-04-09 18:06:05 +0800

    Pictures for icons were updated in "resource"

--- src/ControlClass/APP.java
+++ src/ControlClass/APP.java
@@ -11,7 +11,7 @@ public class APP extends javafx.application.Application{
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../fxml/register.fxml"));
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 1350, 807);
         stage.setScene(scene);
         stage.show();
     }
