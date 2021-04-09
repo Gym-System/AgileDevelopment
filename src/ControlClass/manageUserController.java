9d2021f41d436174ba2637a872318ffa380f027c
GY <guchucheng2000@gmail.com>
2021-04-09 20:07:03 +0800

    Update manage_user.fxml and manageUserController.java

--- src/ControlClass/manageUserController.java
+++ src/ControlClass/manageUserController.java
@@ -1,9 +1,16 @@
 package ControlClass;
 
-import javafx.fxml.FXML;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TextField;
 
 public class manageUserController {
 
@@ -14,7 +21,259 @@ public class manageUserController {
     private URL location;
 
     @FXML
+    private Label email1_mu;
+
+    @FXML
+    private Button button_edit_mu;
+
+    @FXML
+    private TextField text_find_mu;
+
+    @FXML
+    private Hyperlink trainer_hyplink_mu;
+
+    @FXML
+    private Label email3_mu;
+
+    @FXML
+    private Label name2_mu;
+
+    @FXML
+    private Hyperlink regu_mu;
+
+    @FXML
+    private Pagination turn_page;
+
+    @FXML
+    private Button button_delete_mu;
+
+    @FXML
+    private Label password2_mu;
+
+    @FXML
+    private Label password4_mu;
+
+    @FXML
+    private Hyperlink out_hyplink_mu;
+
+    @FXML
+    private Hyperlink income_hyplink_mu;
+
+    @FXML
+    private Label name3_mu;
+
+    @FXML
+    private Label email5_mu;
+
+    @FXML
+    private Label name5_mu;
+
+    @FXML
+    private CheckBox box4_mu;
+
+    @FXML
+    private CheckBox box2_mu;
+
+    @FXML
+    private Label email2_mu;
+
+    @FXML
+    private Label name1_mu;
+
+    @FXML
+    private Label password_mu;
+
+    @FXML
+    private Label password1_mu;
+
+    @FXML
+    private Label password3_mu;
+
+    @FXML
+    private Label password5_mu;
+
+    @FXML
+    private Label email_mu;
+
+    @FXML
+    private Hyperlink user_hyplink_mu;
+
+    @FXML
+    private Label name4_mu;
+
+    @FXML
+    private Label email4_mu;
+
+    @FXML
+    private CheckBox box5_mu;
+
+    @FXML
+    private Button button_find_mu;
+
+    @FXML
+    private Label name_mu;
+
+    @FXML
+    private CheckBox box1_mu;
+
+    @FXML
+    private CheckBox box3_mu;
+
+    @FXML
+    void click_user_hyplink_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_trainer_hyplink_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_income_hyplink_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_regu_hyplink_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_out_hyplink_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name1_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email1_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password1_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name2_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email2_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name3_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name4_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_name5_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email3_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email4_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_email5_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password2_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password3_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password4_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_password5_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_find_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_edit_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_delete_mu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_turn_page(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert email1_mu != null : "fx:id=\"email1_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert button_edit_mu != null : "fx:id=\"button_edit_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert text_find_mu != null : "fx:id=\"text_find_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert trainer_hyplink_mu != null : "fx:id=\"trainer_hyplink_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert email3_mu != null : "fx:id=\"email3_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert name2_mu != null : "fx:id=\"name2_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert regu_mu != null : "fx:id=\"regu_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert turn_page != null : "fx:id=\"turn_page\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert button_delete_mu != null : "fx:id=\"button_delete_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert password2_mu != null : "fx:id=\"password2_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert password4_mu != null : "fx:id=\"password4_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert out_hyplink_mu != null : "fx:id=\"out_hyplink_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert income_hyplink_mu != null : "fx:id=\"income_hyplink_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert name3_mu != null : "fx:id=\"name3_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert email5_mu != null : "fx:id=\"email5_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert name5_mu != null : "fx:id=\"name5_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert box4_mu != null : "fx:id=\"box4_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert box2_mu != null : "fx:id=\"box2_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert email2_mu != null : "fx:id=\"email2_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert name1_mu != null : "fx:id=\"name1_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert password_mu != null : "fx:id=\"password_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert password1_mu != null : "fx:id=\"password1_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert password3_mu != null : "fx:id=\"password3_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert password5_mu != null : "fx:id=\"password5_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert email_mu != null : "fx:id=\"email_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert user_hyplink_mu != null : "fx:id=\"user_hyplink_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert name4_mu != null : "fx:id=\"name4_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert email4_mu != null : "fx:id=\"email4_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert box5_mu != null : "fx:id=\"box5_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert button_find_mu != null : "fx:id=\"button_find_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert name_mu != null : "fx:id=\"name_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert box1_mu != null : "fx:id=\"box1_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
+        assert box3_mu != null : "fx:id=\"box3_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
 
     }
 }
