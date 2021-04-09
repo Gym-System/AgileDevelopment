bc625ccaaac392cb8441e00a97475e630942fdea
GY <guchucheng2000@gmail.com>
2021-04-09 10:03:44 +0800

    没啥事

--- src/ControlClass/APP.java
+++ src/ControlClass/APP.java
@@ -8,8 +8,8 @@ import javafx.stage.Stage;
 public class APP extends javafx.application.Application{
     @Override
     public void start(Stage stage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/register.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("../fxml/register.fxml"));
         Scene scene = new Scene(root, 1350, 807);
         stage.setScene(scene);
         stage.show();
