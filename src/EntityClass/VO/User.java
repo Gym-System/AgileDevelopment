8df54a04debf4b51bde59195be2d00a72a1745c6
inthesum <50326416+inthesum@users.noreply.github.com>
2021-04-12 22:49:27 +0800

    1. Add exerciseTime() in User.java 2. Add calBMI() in PhyData.java

--- src/EntityClass/VO/User.java
+++ src/EntityClass/VO/User.java
@@ -1,10 +1,16 @@
 package EntityClass.VO;
 
 import EntityClass.DAO.impl.HistoryDataDAOImpl;
+import EntityClass.DAO.impl.LiveSessionDAOImpl;
 import EntityClass.DAO.impl.RecVideoDAOImpl;
 import EntityClass.DAO.impl.UserDAOImpl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class User extends Person {
     private double balance = 0.0;
@@ -68,6 +74,123 @@ public class User extends Person {
         star = (star * rateTime + rate) / (rateTime + 1);
         recVideoDAO.changeRecVideoStar(courseId, star);
         recVideoDAO.changeRecVideoRateTime(courseId, rateTime + 1);
+    }
+
+    public int exerciseTime(String timeType) throws ParseException {
+        int time = 0;
+        HistoryDataDAOImpl historyDataDAO = new HistoryDataDAOImpl();
+        ArrayList<HistoryData> historyDatas = historyDataDAO.queryByUserName(getUserName());
+
+        LiveSessionDAOImpl liveSessionDAO = new LiveSessionDAOImpl();
+        RecVideoDAOImpl recVideoDAO = new RecVideoDAOImpl();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+
+        LiveSession liveSession;
+        RecVideo recVideo;
+
+        Date date1, date2;
+
+        if(timeType.equals("day")) {
+            calendar.add(Calendar.DATE,-1);
+            date2 = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).parse(
+                    new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).format(calendar.getTime()));
+
+            for(HistoryData historyData:historyDatas) {
+                if(historyData.getType().equals("live")) {
+                    liveSession = liveSessionDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = liveSession.getStartTime();
+                }
+                else {
+                    recVideo = recVideoDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = recVideo.getUploadedTime();
+                }
+                if(date1.before(date2)) {
+                    historyDatas.remove(historyData);
+                }
+            }
+        }
+        else if(timeType.equals("week")) {
+            calendar.add(Calendar.DATE,-7);
+            date2 = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).parse(
+                    new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).format(calendar.getTime()));
+
+            for(HistoryData historyData:historyDatas) {
+                if(historyData.getType().equals("live")) {
+                    liveSession = liveSessionDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = liveSession.getStartTime();
+                }
+                else {
+                    recVideo = recVideoDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = recVideo.getUploadedTime();
+                }
+                if(date1.before(date2)) {
+                    historyDatas.remove(historyData);
+                }
+            }
+        }
+        else if(timeType.equals("month")) {
+            calendar.add(Calendar.MONTH,-1);
+            date2 = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).parse(
+                    new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).format(calendar.getTime()));
+
+            for(HistoryData historyData:historyDatas) {
+                if(historyData.getType().equals("live")) {
+                    liveSession = liveSessionDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = liveSession.getStartTime();
+                }
+                else {
+                    recVideo = recVideoDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = recVideo.getUploadedTime();
+                }
+                if(date1.before(date2)) {
+                    historyDatas.remove(historyData);
+                }
+            }
+        }
+        else if(timeType.equals("year")) {
+            calendar.add(Calendar.MONTH,-12);
+            date2 = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).parse(
+                    new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.UK).format(calendar.getTime()));
+
+            for(HistoryData historyData:historyDatas) {
+                if(historyData.getType().equals("live")) {
+                    liveSession = liveSessionDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = liveSession.getStartTime();
+                }
+                else {
+                    recVideo = recVideoDAO.queryByCourseId(historyData.getCourseId());
+                    date1 = recVideo.getUploadedTime();
+                }
+                if(date1.before(date2)) {
+                    historyDatas.remove(historyData);
+                }
+            }
+        }
+        time = exerciseTime(historyDatas);
+
+        return time;
+    }
+
+    public int exerciseTime(ArrayList<HistoryData> historyDatas) {
+        int time = 0;
+        LiveSession liveSession;
+        RecVideo recVideo;
+        LiveSessionDAOImpl liveSessionDAO = new LiveSessionDAOImpl();
+        RecVideoDAOImpl recVideoDAO = new RecVideoDAOImpl();
+
+        for(HistoryData historyData:historyDatas) {
+            if(historyData.getType().equals("live")) {
+                liveSession = liveSessionDAO.queryByCourseId(historyData.getCourseId());
+                time += liveSession.getLength();
+            }
+            else {
+                recVideo = recVideoDAO.queryByCourseId(historyData.getCourseId());
+                time += recVideo.getLength();
+            }
+        }
+        return time;
     }
 
     public String[] toStrArray() {
