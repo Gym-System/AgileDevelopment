8cae86a55034d6bf8dbd60388df54db6da980cf6
Aquarius <liqichen@bupt.edu.cn>
2021-04-12 15:15:41 +0800

    manage user infomation 1.0

--- src/ControlClass/manageUserController.java
+++ src/ControlClass/manageUserController.java
@@ -1,18 +1,48 @@
 package ControlClass;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import EntityClass.DAO.impl.UserDAOImpl;
+import EntityClass.VO.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.Pagination;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.util.Callback;
 
-public class manageUserController {
+import javax.swing.*;
+
+public class manageUserController  {
+
+
+    Label name0 = new Label();
+    Label name1 = new Label();
+    Label name2 = new Label();
+    Label name3 = new Label();
+    Label name4 = new Label();
+    Label email0 = new Label();
+    Label email1 = new Label();
+    Label email2 = new Label();
+    Label email3 = new Label();
+    Label email4 = new Label();
+    Label password0 = new Label();
+    Label password1 = new Label();
+    Label password2 = new Label();
+    Label password3 = new Label();
+    Label password4 = new Label();
+    Label[] names = new Label[5];
+    Label[] emails = new Label[5];
+    Label[] passwords = new Label[5];
+
+
 
     @FXML
     private ResourceBundle resources;
@@ -221,6 +251,7 @@ public class manageUserController {
 
     @FXML
     void click_find_mu(ActionEvent event) {
+        UserDAOImpl userDAO = new UserDAOImpl();
 
     }
 
@@ -236,7 +267,24 @@ public class manageUserController {
 
     @FXML
     void click_turn_page(ActionEvent event) {
+//        turn_page.currentPageIndexProperty();
+        turn_page.setPageFactory(new Callback<Integer, Node>() {
+            @Override
+            public Node call(Integer param) {
+                System.out.println("当前的页面索引 = " + param.intValue());
+                VBox box = new VBox(5);
+                for (int i = param.intValue(); i<param.intValue() + 5 ; i++) {
+                    VBox element = new VBox();
+                    Label text = new Label("用户" + i);
+                    element.getChildren().addAll(text);
+                    box.getChildren().addAll(element);
+                }
 
+
+                Node node = null;
+                return box;
+            }
+        });
     }
 
     @FXML
@@ -275,5 +323,230 @@ public class manageUserController {
         assert box1_mu != null : "fx:id=\"box1_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
         assert box3_mu != null : "fx:id=\"box3_mu\" was not injected: check your FXML file 'manage_user.fxml'.";
 
+
+        UserDAOImpl userDAO = new UserDAOImpl();
+        ArrayList<User> users = userDAO.queryAll();
+        turn_page.setPageCount((int) Math.ceil(users.size()/5.0));
+        for (User user:users){
+            user.getUserName();
+        }
+        names[0] = name1_mu;
+        names[1] = name2_mu;
+        names[2] = name3_mu;
+        names[3] = name4_mu;
+        names[4] = name5_mu;
+        emails[0] = email1_mu;
+        emails[1] = email2_mu;
+        emails[2] = email3_mu;
+        emails[3] = email4_mu;
+        emails[4] = email5_mu;
+        passwords[0] = password1_mu;
+        passwords[1] = password2_mu;
+        passwords[2] = password3_mu;
+        passwords[3] = password4_mu;
+        passwords[4] = password5_mu;
+
+
+        turn_page.setPageFactory(new Callback<Integer, Node>() {
+            @Override
+            public Node call(Integer param) {
+                VBox box =new VBox();
+                int limit = 5;
+                if (param.intValue() == users.size()/5) {
+                    limit = users.size()%5;
+                }
+
+                if(param.intValue() < limit){
+                    for (int i =0; i <limit; i++){
+                        names[i].setText(users.get(5 * param.intValue() + i).getUserName());
+                        emails[i].setText(users.get(5 * param.intValue() + i).getEmail());
+                        passwords[i].setText(users.get(5 * param.intValue() + i).getPassword());
+                    }
+
+                } else {
+                    for (int i = limit; i<5; i++){
+                        names[i].setText("");
+                        emails[i].setText("");
+                        passwords[i].setText("");
+                    }
+
+                }
+
+//                System.out.println("当前的页面索引 = " + param.intValue());
+//
+//
+//                Node node = null;
+
+//                if (param.intValue() == (int)users.size()/5){
+//                    System.out.println(param.intValue());
+//                }
+//                GridPane gridPane;
+//                GridPane gridPane = new GridPane();
+//                gridPane.setHgap(6);
+//                gridPane.setVgap(4);
+//                gridPane.setVgap(62);
+//                gridPane.setHgap(150);
+//
+////                RowConstraints rowConstraints0 = new RowConstraints();
+////                rowConstraints0.setPrefHeight(30);
+////                gridPane.getRowConstraints().add(rowConstraints0);
+//
+//                gridPane.setAlignment(Pos.CENTER);
+//                page(param.intValue());
+//                int length = users.size();
+//                names[0] = name0;
+//                names[1] = name1;
+//                names[2] = name2;
+//                names[3] = name3;
+//                names[4] = name4;
+//                emails[0] = email0;
+//                emails[1] = email1;
+//                emails[2] = email2;
+//                emails[3] = email3;
+//                emails[4] = email4;
+//                passwords[0] = password0;
+//                passwords[1] = password1;
+//                passwords[2] = password2;
+//                passwords[3] = password3;
+//                passwords[4] = password4;
+//
+//
+//
+//                System.out.println();
+//                int limit = 5;
+//                if (param.intValue() == users.size()/5) limit = length%5;
+//                System.out.println("1111111111");
+//
+//                for (int i =0; i < limit; i++){
+//                    System.out.println(users.get(5 * param.intValue() + i).getUserName());
+//                    names[i].setText(users.get(5 * param.intValue() + i).getUserName());
+//                    emails[i].setText(users.get(5 * param.intValue() + i).getEmail());
+//                    passwords[i].setText(users.get(5 * param.intValue() + i).getPassword());
+//                    gridPane.add(names[i],0,i);
+//                    gridPane.add(emails[i],1,i);
+//                    gridPane.add(passwords[i],2,i);
+//                }
+//                if (param.intValue() > users.size()/5){
+//                    return null;
+//                }
+
+//                Label label1 = new Label(user.getUserName());
+//                label1.setAlignment(Pos.CENTER);
+//                gridPane.setPrefSize(870,424);
+//                int page = param.intValue() *5;
+//                if (param.intValue() > users.size()/5){
+//                    return null;
+//                } else if(param.intValue() < users.size()/5) {
+//                    for (int i = page; i < page +5; i++){
+//
+//                    }
+//                } else if (param.intValue() == users.size()/5){
+//                    for (int i = page; i< page + users.size()%5;){
+//
+//                    }
+//
+//                }
+
+//                for (int i=0; i<100;i++){
+//                    if (i == 0) {
+//                        for (int j =0; j<5; j++) {
+//                            name0.setText(users.get(j).getUserName());
+//                            gridPane.add(new Label(users.get(j).getUserName()),0,j);
+//                            gridPane.add(name0,0,j);
+//                            gridPane.add(new Label(users.get(j).getEmail()),1,j);
+//                            gridPane.add(new Label(users.get(j).getPassword()),2,j);
+//                        }
+//                    }
+//                    if (i >=1 && i<=param.intValue()) {
+//                        for (int j =0; j<users.size()%5; j++) {
+//                            gridPane.add(new Label(users.get((j + i*5)).getUserName()),0,j);
+//                            gridPane.add(new Label(users.get((j + i*5)).getEmail()),1,j);
+//                            gridPane.add(new Label(users.get((j + i*5)).getPassword()),2,j);
+//                        }
+//                    }
+//                }
+
+
+//                for (int j = 0; j <= (int)users.size()/5; j++) {
+//                    if (j == (int)users.size()/5) {
+//                        for (int i =0; i<5; i++) {
+//                            gridPane.add(new Label(users.get(j * 5 +users.size()%5 - 1).getUserName()),0,i);
+//                            gridPane.add(new Label(users.get(j * 5 +users.size()%5 - 1).getEmail()),1,i);
+//                            gridPane.add(new Label(users.get(j * 5 +users.size()%5 - 1).getPassword()),2,i);
+//                        }
+//                    }else {
+//                        for(int i=0;i< 5 ;i++){
+//                            //                    Separator separator = new Separator();
+//                            //                    separator.setLayoutX(100.0);
+//                            //                    separator.setLayoutY(100.0);
+//                            //                    separator.setPrefHeight(0);
+//                            //                    separator.setPrefWidth(800);
+//                            //                    gridPane.add(separator,0,0);
+//                            //                    GridPane.setHgrow(label,Priority.ALWAYS);
+//                            //                    GridPane.setVgrow(label, Priority.ALWAYS);
+//                            if (param.intValue() == (int)users.size()/5){
+//                                System.out.println(param.intValue());
+//                            }
+//                            gridPane.add(new Label(users.get(i).getUserName()),0,i);
+//                            gridPane.add(new Label(users.get(i).getEmail()),1,i);
+//                            gridPane.add(new Label(users.get(i).getPassword()),2,i);
+//                            //                    gridPane.getColumnConstraints().add(new ColumnConstraints(150));
+//                            //                    gridPane.getRowConstraints().add(new RowConstraints(50));
+//
+//                        }
+//                    }
+//
+//                }
+//                System.out.println(getClass().getResource("../fxml/manager_userInfo.fxml"));
+//                Node root = null;
+//                try {
+//                    root = FXMLLoader.load(getClass().getResource("../fxml/manager_userInfo.fxml"));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                VBox vBox = new VBox();
+//                vBox.getChildren().add(root);
+//                return vBox;
+
+//                VBox box = new VBox(5);
+//                for (int i = param.intValue(); i<param.intValue() + 5 ; i++) {
+//                    VBox element = new VBox();
+//                    Label text = new Label("用户" + i);
+//                    element.getChildren().addAll(text);
+//                    box.getChildren().addAll(element);
+//                }
+//
+//
+//                Node node = null;
+//                return box;
+            return box;
+            }
+        });
     }
+
+//    public void page(int pageNo) {
+//        UserDAOImpl userDAO = new UserDAOImpl();
+//        ArrayList<User> users = userDAO.queryUser();
+//        int length = users.size();
+//        names[0] = name0;
+//        names[1] = name1;
+//        names[2] = name2;
+//        names[3] = name3;
+//        names[4] = name4;
+//        if (pageNo < users.size()/5) {
+//            for (int i =0; i <5; i++){
+//                names[i].setText(users.get(5 * pageNo + i).getUserName());
+//
+//            }
+//        }
+//        if (pageNo == users.size()/5) {
+//            for (int i =0; i < length%5; i++){
+//                names[i].setText(users.get(5 * pageNo + i).getUserName());
+//            }
+//        }
+//
+//    }
+
+
 }
