94db30dc435ce4afd3a240170782d23b49068142
Positive Zhang <boyangyang523@163.com>
2021-04-12 14:31:15 +0800

    Adjust the information display on the UserInfo.fxml

--- src/ControlClass/userInfoController.java
+++ src/ControlClass/userInfoController.java
@@ -1,11 +1,14 @@
 package ControlClass;
+import EntityClass.DAO.LiveSessionDAO;
 import EntityClass.DAO.UserDAO;
-import EntityClass.DAO.impl.PhyDataDAOImpl;
-import EntityClass.DAO.impl.UserDAOImpl;
+import EntityClass.DAO.impl.*;
+import EntityClass.VO.LiveSession;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class userInfoController {
 
@@ -53,22 +56,38 @@ public class userInfoController {
         String username = passValue.getValue();
         UserDAOImpl userDAO = new UserDAOImpl();
         PhyDataDAOImpl phyDataDAO = new PhyDataDAOImpl();
+        RecVideoDAOImpl recVideoDAO = new RecVideoDAOImpl();
+        LiveSessionDAO liveSessionDAO = new LiveSessionDAOImpl();
+        PreUserDAOImpl preUserDAO = new PreUserDAOImpl();
         name.setText( userDAO.queryByUserName(username).getUserName());
-        //ID在csv文件中并没有存储
         gender.setText(userDAO.queryByUserName(username).getGender());
         java.text.SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd ");
         String date = formatter.format(userDAO.queryByUserName(username).getDoB());
         birthday.setText(date);
-        //city在csv文件中并没有存储
         email.setText(userDAO.queryByUserName(username).getEmail());
-        level.setText("You are not a member!");
+        if(preUserDAO.queryByUserName(username)==null){
+            level.setText(" Not a member!");
+        }else{
+            level.setText(Integer.toString(preUserDAO.queryByUserName(username).getUserType()));
+        }
         balance.setText(Double.toString(userDAO.queryByUserName(username).getBalance()));
-        weight.setText(Double.toString(phyDataDAO.queryByUserName(username).getWeight()));
-        height.setText(Double.toString(phyDataDAO.queryByUserName(username).getHeight()));
+        weight.setText(Double.toString(phyDataDAO.queryByUserName(username).getWeight())+" kg");
+        height.setText(Double.toString(phyDataDAO.queryByUserName(username).getHeight())+" cm");
         double result=(10000*phyDataDAO.queryByUserName(username).getWeight())/(phyDataDAO.queryByUserName(username).getHeight()*phyDataDAO.queryByUserName(username).getHeight());
         bmi.setText(Double.toString(result));
-        totalTime.setText(Integer.toString(phyDataDAO.queryByUserName(username).getExperience()));//这个是一周的总运动时间吗？？？
-        energy.setText("这一栏对应的是数据库的啥");
+        ArrayList<LiveSession> liveSessions = liveSessionDAO.queryByUserName("aaa");
+        int time1 = 0;
+        System.out.println(liveSessions);
+//        if(liveSessions.isEmpty()) {
+//            totalTime.setText(String.valueOf(time1));
+//        }
+//        else{
+//            for(LiveSession liveSession1:liveSessions) {
+//                time1=time1+liveSession1.getLength();
+//            }
+//            totalTime.setText(String.valueOf(time1));
+//        }
+
 
 
     }
