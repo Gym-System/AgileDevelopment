3b9f88f9eb8c8c3d27f5fcd9886f81b46af9df3d
GY <guchucheng2000@gmail.com>
2021-05-10 18:12:07 +0800

    Update manage_trainer.fxml, manage_income.fxml, and manageIncomeController.java

---
+++ src/ControlClass/manageIncomeController.java
@@ -0,0 +1,198 @@
+package ControlClass;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TextField;
+
+public class manageIncomeController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label record_hour_mi;
+
+    @FXML
+    private Pagination turn_page_mi;
+
+    @FXML
+    private Hyperlink user_hyplink_mi;
+
+    @FXML
+    private Label live_hour_mi;
+
+    @FXML
+    private Label c_income3_mi;
+
+    @FXML
+    private Label c_income1_mi;
+
+    @FXML
+    private Button button_find_mi;
+
+    @FXML
+    private TextField text_find_mi;
+
+    @FXML
+    private Label name1_mi;
+
+    @FXML
+    private Label record_hour3_mi;
+
+    @FXML
+    private Label record_hour1_mi;
+
+    @FXML
+    private Label name3_mi;
+
+    @FXML
+    private Hyperlink trainer_hyplink_mi;
+
+    @FXML
+    private Hyperlink out_hyplink_mi;
+
+    @FXML
+    private Label award4_mi;
+
+    @FXML
+    private Label award2_mi;
+
+    @FXML
+    private Label live_hour4_mi;
+
+    @FXML
+    private Label live_hour2_mi;
+
+    @FXML
+    private Hyperlink income_hyplink_mi;
+
+    @FXML
+    private Label c_income4_mi;
+
+    @FXML
+    private Label c_income2_mi;
+
+    @FXML
+    private ChoiceBox<?> chosice_sort_mi;
+
+    @FXML
+    private Label award_mi;
+
+    @FXML
+    private Label award1_mi;
+
+    @FXML
+    private Label record_hour4_mi;
+
+    @FXML
+    private Label c_income_mi;
+
+    @FXML
+    private Label name2_mi;
+
+    @FXML
+    private Label name_mi;
+
+    @FXML
+    private Label record_hour2_mi;
+
+    @FXML
+    private Label name4_mi;
+
+    @FXML
+    private Hyperlink regu_mi;
+
+    @FXML
+    private Label award3_mi;
+
+    @FXML
+    private Label live_hour1_mi;
+
+    @FXML
+    private Label live_hour3_mi;
+
+    @FXML
+    void click_user_hyplink_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_trainer_hyplink_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_income_hyplink_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_regu_hyplink_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_out_hyplink_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_find_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void click_turn_page_mi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert record_hour_mi != null : "fx:id=\"record_hour_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert turn_page_mi != null : "fx:id=\"turn_page_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert user_hyplink_mi != null : "fx:id=\"user_hyplink_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert live_hour_mi != null : "fx:id=\"live_hour_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert c_income3_mi != null : "fx:id=\"c_income3_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert c_income1_mi != null : "fx:id=\"c_income1_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert button_find_mi != null : "fx:id=\"button_find_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert text_find_mi != null : "fx:id=\"text_find_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert name1_mi != null : "fx:id=\"name1_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert record_hour3_mi != null : "fx:id=\"record_hour3_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert record_hour1_mi != null : "fx:id=\"record_hour1_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert name3_mi != null : "fx:id=\"name3_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert trainer_hyplink_mi != null : "fx:id=\"trainer_hyplink_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert out_hyplink_mi != null : "fx:id=\"out_hyplink_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert award4_mi != null : "fx:id=\"award4_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert award2_mi != null : "fx:id=\"award2_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert live_hour4_mi != null : "fx:id=\"live_hour4_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert live_hour2_mi != null : "fx:id=\"live_hour2_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert income_hyplink_mi != null : "fx:id=\"income_hyplink_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert c_income4_mi != null : "fx:id=\"c_income4_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert c_income2_mi != null : "fx:id=\"c_income2_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert chosice_sort_mi != null : "fx:id=\"chosice_sort_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert award_mi != null : "fx:id=\"award_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert award1_mi != null : "fx:id=\"award1_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert record_hour4_mi != null : "fx:id=\"record_hour4_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert c_income_mi != null : "fx:id=\"c_income_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert name2_mi != null : "fx:id=\"name2_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert name_mi != null : "fx:id=\"name_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert record_hour2_mi != null : "fx:id=\"record_hour2_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert name4_mi != null : "fx:id=\"name4_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert regu_mi != null : "fx:id=\"regu_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert award3_mi != null : "fx:id=\"award3_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert live_hour1_mi != null : "fx:id=\"live_hour1_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+        assert live_hour3_mi != null : "fx:id=\"live_hour3_mi\" was not injected: check your FXML file 'manage_income.fxml'.";
+
+    }
+}
