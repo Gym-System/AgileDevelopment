5d5d0aea7d7655df923a1523761662325d294ddd
Aquarius <liqichen@bupt.edu.cn>
2021-05-31 13:57:20 +0800

    add

--- src/EntityClass/VO/Trainer.java
+++ src/EntityClass/VO/Trainer.java
@@ -10,20 +10,75 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * javadoc of Trainer class
+ * @author Kaiyi Zhao
+ * @version 1.0
+ */
 public class Trainer extends Staff {
     private double salary = 0.0;
     private String label = "To be added";
     private String CV = "To be added";
     private double price = 300.0;
 
+    /**
+     * This constructor contains major params needed to make a instance of Trainer class.
+     * @param userName The userName of a trainer
+     * @param password The password of a trainer
+     * @param email The email of a trainer
+     */
     public Trainer(String userName, String password, String email) {
         super(userName, password, email);
     }
 
+    /**
+     * This constructor contains major params needed to make a instance of Trainer class.
+     * @param userName The userName of a trainer
+     * @param password The password of a trainer
+     * @param email The email of a trainer
+     * @param gender The gender of a trainer
+     * @param telNo The telephone number of a trainer
+     * @param doB The date of birth of a trainer
+     * @param companyName Name of the company trainer is in
+     */
     public Trainer(String userName, String password, String email, String gender, String telNo, Date doB, String companyName) {
         super(userName, password, email, gender, telNo, doB, companyName);
     }
 
+    /**
+     * This constructor contains all params needed to make a instance of Trainer class.
+     * @param userName The userName of a trainer
+     * @param password The password of a trainer
+     * @param email The email of a trainer
+     * @param gender The gender of a trainer
+     * @param telNo The telephone number of a trainer
+     * @param doB The date of birth of a trainer
+     * @param companyName Name of the company trainer is in
+     * @param salary The salary of a trainer
+     * @param CV The cv of a trainer
+     * @param price The price of a trainer's live session
+     */
+    public Trainer(String userName, String password, String email, String gender, String telNo, Date doB, String companyName, double salary, String CV, double price) {
+        super(userName, password, email, gender, telNo, doB, companyName);
+        this.salary = salary;
+        this.CV = CV;
+        this.price = price;
+    }
+
+    /**
+     * This constructor contains all params needed to make a instance of Trainer class.
+     * @param userName The userName of a trainer
+     * @param password The password of a trainer
+     * @param email The email of a trainer
+     * @param gender The gender of a trainer
+     * @param telNo The telephone number of a trainer
+     * @param doB The date of birth of a trainer
+     * @param companyName Name of the company trainer is in
+     * @param salary The salary of a trainer
+     * @param label The label of a trainer
+     * @param CV The cv of a trainer
+     * @param price The price of a trainer's live session
+     */
     public Trainer(String userName, String password, String email, String gender, String telNo, Date doB, String companyName, double salary, String label, String CV, double price) {
         super(userName, password, email, gender, telNo, doB, companyName);
         this.salary = salary;
@@ -32,42 +87,75 @@ public class Trainer extends Staff {
         this.price = price;
     }
 
+    /**
+     * The getter method of salary
+     * @return salary
+     */
     public double getSalary() {
         return salary;
     }
 
+    /**
+     * The setter method of salary
+     * @param salary The salary of a trainer
+     */
     public void setSalary(double salary) {
         this.salary = salary;
     }
 
+    /**
+     * The getter method of label
+     * @return label
+     */
     public String[] getLabel() {
         return label.split(";");
     }
 
+    /**
+     * The setter method of label
+     * @param label The label of a trainer
+     */
     public void setLabel(String label) {
         this.label = label;
     }
 
+    /**
+     * The getter method of CV
+     * @return CV
+     */
     public String getCV() {
         return CV;
     }
 
+    /**
+     * The setter method of CV
+     * @param CV The CV of a trainer
+     */
     public void setCV(String CV) {
         this.CV = CV;
     }
 
+    /**
+     * The getter method of price
+     * @return price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * The setter method of price
+     * @param price The price of a trainer's live session
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
-    public String getCVStrArray() {
-        return CV;
-    }
-
+    /**
+     * This method show trainer's calender
+     * @return A array list of live sessions
+     * @throws ParseException
+     */
     public ArrayList<LiveSession> showCalender() throws ParseException {
         LiveSessionDAOImpl liveSessionDAO = new LiveSessionDAOImpl();
         ArrayList<LiveSession> liveSessions = liveSessionDAO.queryByTrainerName(getUserName());
@@ -92,6 +180,11 @@ public class Trainer extends Staff {
         return liveSessions1;
     }
 
+    /**
+     * This method calculate total recorded time of a trainer
+     * @return Total recorded time of a trainer
+     * @throws ParseException
+     */
     public int getRecTime() throws ParseException {
         int recTime = 0;
         Calendar calendar = Calendar.getInstance();
@@ -109,6 +202,11 @@ public class Trainer extends Staff {
         return recTime;
     }
 
+    /**
+     * This method calculate total live session time of a trainer
+     * @return Total live session of a trainer
+     * @throws ParseException
+     */
     public int getLiveTime() throws ParseException {
         int liveTime = 0;
         Calendar calendar = Calendar.getInstance();
@@ -126,6 +224,11 @@ public class Trainer extends Staff {
         return liveTime;
     }
 
+    /**
+     * This method calculate total gift a trainer get from lectures
+     * @return total gift a trainer get from lectures
+     * @throws ParseException
+     */
     public int getGift() throws ParseException {
         int gift = 0;
         Calendar calendar = Calendar.getInstance();
@@ -155,6 +258,10 @@ public class Trainer extends Staff {
         return gift;
     }
 
+    /**
+     * This method organize instance variables of Trainer class into a string array
+     * @return A string array contain instance variables of Trainer class
+     */
     @Override
     public String[] toStrArray() {
         String text = "" + super.getUserName() + "," + super.getPassword() + "," + super.getEmail() + "," + super.getGender() + "," +
@@ -162,7 +269,10 @@ public class Trainer extends Staff {
         return text.split(",");
     }
 
-
+    /**
+     * This method organize instance variables of Trainer class into a readable way
+     * @return A string contains instance variables of Trainer class
+     */
     @Override
     public String toString() {
         return "Trainer{" +
